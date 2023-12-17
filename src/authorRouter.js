de133f3afa3b799ae6dabceb9a1554b1ed4fbd5a
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-12-18 00:40:15 +0100

    up

--- src/authorRouter.js
+++ src/authorRouter.js
@@ -32,7 +32,9 @@ authorRouter.get(
       expiresIn: "1h",
     });
 
-    res.redirect(`http://localhost:3000?token=${token}&userId=${req.user.id}`);
+    res.redirect(
+      `${process.env.REACT_APP_BACKEND_ENDPOINT}?token=${token}&userId=${req.user.id}`
+    );
   }
 );
 
